16d72fa5a5b6641ec659a41089b1cc908d8042f5
Alfred Assey M <asseym@gmail.com>
2013-01-21 11:50:52 +0300

    initial commit

---
+++ djwiki/urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls import patterns, include, url
+from wiki.urls import get_pattern as get_wiki_pattern
+from django_notify.urls import get_pattern as get_notify_pattern
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djwiki.views.home', name='home'),
+    # url(r'^djwiki/', include('djwiki.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^notify/', get_notify_pattern()),
+    (r'', get_wiki_pattern())
+)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+   
